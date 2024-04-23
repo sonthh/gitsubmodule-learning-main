63955a6b52d22d4d1230f67c17457e9d5c7c941d
sonthh <tranhuuhongson@gmail.com>
2024-04-23 13:46:29 +0700

    add dotenv

--- src/app.js
+++ src/app.js
@@ -1,7 +1,10 @@
 const clsx = require("clsx");
+require("dotenv").config();
+
 const { reportsPage } = require("./modules/reports/page");
 
 console.log("Reports: ", reportsPage);
 console.log("App use clsx: ", clsx("foo", true && "bar", "baz"));
+console.log("App use .enn auth=" + process.env.AUTHOR);
 
 console.log("Main app running!");
