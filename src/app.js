b16c79fb0737bb1f2672d795065879eba82fa678
sonthh <tranhuuhongson@gmail.com>
2024-04-23 13:31:45 +0700

    add clsx package

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
+const clsx = require("clsx");
 const { reportsPage } = require("./modules/reports/page");
 
 console.log("Reports: ", reportsPage);
+console.log("App use clsx: ", clsx("foo", true && "bar", "baz"));
 
 console.log("Main app running!");
