9989c339de46c20aadcd3179b2a893b9bfbf173e
sonthh <tranhuuhongson@gmail.com>
2024-04-23 11:13:17 +0700

    add shared module

--- src/modules/reports/page.js
+++ src/modules/reports/page.js
@@ -1,5 +1,10 @@
+const { component1, component2 } = require("../../shared/components");
+const { useHook1, useHook2 } = require("../../shared/hooks");
+
 const reportsPage = {
   name: "reports",
+  components: [component1, component2],
+  hooks: [useHook1, useHook2],
 };
 
 module.exports = {
